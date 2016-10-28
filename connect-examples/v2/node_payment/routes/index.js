70b16e76cac3946500a745fe4ebc9cbd68390fa9
Tristan Sokol <tristans@squareup.com>
2016-10-28 09:51:50 -0700

    Updated Node sample to use first location with credit card processing instead of first location returned by the API

--- connect-examples/v2/node_payment/routes/index.js
+++ connect-examples/v2/node_payment/routes/index.js
@@ -12,7 +12,7 @@ var product_cost = {"001": 100, "002": 4900, "003": 500000}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Node.js Implementation', 'square_application_id': config.squareApplicationId });
+	res.render('index', { title: 'Express Node.js Implementation', 'square_application_id': config.squareApplicationId });
 });
 
 router.post('/charges/charge_card', function(req,res,next){
@@ -24,31 +24,41 @@ router.post('/charges/charge_card', function(req,res,next){
 		'Accept': 'application/json'
 	})
 	.end(function (response) {
-	  location = response.body.locations[0];
-	  
-	  var token = require('crypto').randomBytes(64).toString('hex');
-	  
-	  //Check if product exists
-	  if (!product_cost.hasOwnProperty(request_params.product_id)) {
-	  	return res.json({status: 400, errors: [{"detail": "Product Unavailable"}] })
-	  }
 
-	  //Make sure amount is a valid integer
-	  var amount = product_cost[request_params.product_id]
-	  
-	  request_body = {
-	  	card_nonce: request_params.nonce,
-	  	amount_money: {
-	  		amount: amount,
-	  		currency: 'USD'
-	  	},
-	  	idempotency_key: token
-	  }
-	  unirest.post(base_url + '/locations/' + location.id + "/transactions")
-	  .headers({
-		'Authorization': 'Bearer ' + config.squareAccessToken,
-		'Accept': 'application/json',
-		'Content-Type': 'application/json'
+		for (var i = response.body.locations.length - 1; i >= 0; i--) {
+			if(response.body.locations[i].capabilities.indexOf("CREDIT_CARD_PROCESSING")>-1){
+				location = response.body.locations[i];
+				break;
+			}
+			if(i==0){
+				return res.json({status: 400, errors: [{"detail": "No locations have credit card processing available."}] })
+			}
+		}
+		
+
+		var token = require('crypto').randomBytes(64).toString('hex');
+
+		//Check if product exists
+		if (!product_cost.hasOwnProperty(request_params.product_id)) {
+			return res.json({status: 400, errors: [{"detail": "Product Unavailable"}] })
+		}
+
+		//Make sure amount is a valid integer
+		var amount = product_cost[request_params.product_id]
+
+		request_body = {
+			card_nonce: request_params.nonce,
+			amount_money: {
+				amount: amount,
+				currency: 'USD'
+			},
+			idempotency_key: token
+		}
+		unirest.post(base_url + '/locations/' + location.id + "/transactions")
+		.headers({
+			'Authorization': 'Bearer ' + config.squareAccessToken,
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
 		})
 		.send(request_body)
 		.end(function(response){
