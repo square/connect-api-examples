30dd2e161a3c5cb9669c4eb7f9a55dc320cc1839
Francisco Rojas <frojas@squareup.com>
2017-05-31 07:41:23 -0700

    upgrade python example to squareconnect v2.1

--- connect-examples/v2.1/python_payment/cgi-bin/process_card.py
+++ connect-examples/v2.1/python_payment/cgi-bin/process_card.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
-from __future__ import print_function 
+from __future__ import print_function
 import uuid
 import cgi
 
 import squareconnect
 from squareconnect.rest import ApiException
-from squareconnect.apis.transaction_api import TransactionApi
+from squareconnect.apis.transactions_api import TransactionsApi
+from squareconnect.apis.locations_api import LocationsApi
 
-# Create instance of FieldStorage 
+# Create instance of FieldStorage
 form = cgi.FieldStorage()
 # Get data from fields
 nonce = form.getvalue('nonce')
 
 # The access token to use in all Connect API requests. Use your *sandbox* access
 # token if you're just testing things out.
-access_token = 'REPLACE_ME'
+squareconnect.configuration.access_token = 'REPLACE_ME'
 
 # The ID of the business location to associate processed payments with.
 # See [Retrieve your business's locations]
 # (https://docs.connect.squareup.com/articles/getting-started/#retrievemerchantprofile)
 # for an easy way to get your business's location IDs.
 # If you're testing things out, use a sandbox location ID.
-location_id = 'REPLACE_ME' 
+location_id = 'REPLACE_ME'
 
-api_instance = TransactionApi()
+api_instance = TransactionsApi()
 
 # Every payment you process with the SDK must have a unique idempotency key.
 # If you're unsure whether a particular payment succeeded, you can reattempt
@@ -39,10 +40,10 @@ body = {'idempotency_key': idempotency_key, 'card_nonce': nonce, 'amount_money':
 
 
 # The SDK throws an exception if a Connect endpoint responds with anything besides
-# a 200-level HTTP code. This block catches any exceptions that occur from the request.  
+# a 200-level HTTP code. This block catches any exceptions that occur from the request.
 try:
   # Charge
-  api_response = api_instance.charge(access_token, location_id, body)
+  api_response = api_instance.charge(location_id, body)
   res = api_response.transaction
 except ApiException as e:
   res = "Exception when calling TransactionApi->charge: {}".format(e)
