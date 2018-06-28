f01c6ffef48ead8c7f957f8ab8d3cde5834e9095
Stephen Josey <sjosey@squareup.com>
2018-06-28 14:24:29 -0700

    Added error handling for null or empty amount.

--- connect-examples/v2/php_checkout/checkout.php
+++ connect-examples/v2/php_checkout/checkout.php
@@ -15,7 +15,7 @@
   \SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($access_token);
 
   // make sure we actually are on a POST with an amount
-  if (isset($_POST["amount"])) {
+  if (isset($_POST["amount"]) && !empty($_POST["amount"])) {
     try {
       $checkout_api = new \SquareConnect\Api\CheckoutApi();
       $request_body = new \SquareConnect\Model\CreateCheckoutRequest(
@@ -44,5 +44,8 @@
     // this redirects to the Square hosted checkout page
     header("Location: ".$response->getCheckout()->getCheckoutPageUrl());
     exit();
+  } else {
+    // if no amount was set, just go back home.
+    header("Location: /");
   }
 ?>
