ead12d73bee31df30eb50981c3351cc53719d9d5
Stephen Josey <sjosey@squareup.com>
2018-06-29 11:48:03 -0700

    Moved IS_PROD to env file. Updated Square-Connect.

--- connect-examples/v2/php_checkout/checkout.php
+++ connect-examples/v2/php_checkout/checkout.php
@@ -1,16 +1,14 @@
 <?php
   require 'vendor/autoload.php';
-  // Change to true if production, false if sandbox
-  define("IS_PROD", false);
 
   // dotenv is used to read from the '.env' file created
   $dotenv = new Dotenv\Dotenv(__DIR__);
   $dotenv->load();
 
-  $access_token = (IS_PROD)  ?  $_ENV["PROD_ACCESS_TOKEN"]
-                             :  $_ENV["SANDBOX_ACCESS_TOKEN"];
-  $location_id =  (IS_PROD)  ?  $_ENV["PROD_LOCATION_ID"]
-                             :  $_ENV["SANDBOX_LOCATION_ID"];
+  $access_token = ($_ENV["USE_PROD"])  ?  $_ENV["PROD_ACCESS_TOKEN"]
+                                       :  $_ENV["SANDBOX_ACCESS_TOKEN"];
+  $location_id =  ($_ENV["USE_PROD"])  ?  $_ENV["PROD_LOCATION_ID"]
+                                       :  $_ENV["SANDBOX_LOCATION_ID"];
   // setup authorization
   \SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($access_token);
 
