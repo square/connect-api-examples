cdf6d8ef3e26c01999d38cde0e840536b444cd27
Stephen Josey <sjosey@squareup.com>
2018-07-02 09:28:07 -0700

    Fix PHP Checkout example (#61)
    Fixed PHP Checkout example.

---
+++ connect-examples/v2/php_checkout/README.md
@@ -0,0 +1,29 @@
+Square Checkout Demo
+=========================
+
+This is a simple example application that utilizes Square's Checkout API. This examples does assume you are familiar with PHP development.
+
+It takes a single payment, declared by the user, and creates an order to use in the Checkout API.
+
+To get it running:
+
+* Clone/download to your local computer.
+* Place your credentials in `.env`
+  * <b>WARNING</b>: never upload .env with your credential/access_token
+* In `checkout.php` there's a variable, `IS_PROD` to switch between production and sandbox; change it based on your needs.
+* Downloaded composer (https://getcomposer.org/download/)
+* Run the following command in your terminal to download the required packages (in the directory you copied the project to):
+```
+php composer.phar install
+```
+* Run the following command in your terminal to start your server:
+```
+php -S localhost:8888
+```
+
+This will start the server on `localhost:8888`, which you can navigate to in your favorite browser.
+
+For more information about Checkout please visit:
+* https://docs.connect.squareup.com/payments/checkout/overview
+* https://docs.connect.squareup.com/api/connect/v2#navsection-checkout
+* https://github.com/square/connect-php-sdk/blob/master/docs/Api/CheckoutApi.md
---
+++ connect-examples/v2/php_checkout/checkout.php
@@ -0,0 +1,49 @@
+<?php
+  require 'vendor/autoload.php';
+
+  // dotenv is used to read from the '.env' file created
+  $dotenv = new Dotenv\Dotenv(__DIR__);
+  $dotenv->load();
+
+  $access_token = ($_ENV["USE_PROD"])  ?  $_ENV["PROD_ACCESS_TOKEN"]
+                                       :  $_ENV["SANDBOX_ACCESS_TOKEN"];
+  $location_id =  ($_ENV["USE_PROD"])  ?  $_ENV["PROD_LOCATION_ID"]
+                                       :  $_ENV["SANDBOX_LOCATION_ID"];
+  // setup authorization
+  \SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($access_token);
+
+  // make sure we actually are on a POST with an amount
+  if (isset($_POST["amount"]) && !empty($_POST["amount"])) {
+    try {
+      $checkout_api = new \SquareConnect\Api\CheckoutApi();
+      $request_body = new \SquareConnect\Model\CreateCheckoutRequest(
+        [
+          "idempotency_key" => uniqid(),
+          "order" => [
+            "line_items" => [
+            [
+              "name" => "Test Payment",
+              "quantity" => "1",
+              "base_price_money" => [
+                // multiply by 100 due to it being in cents
+                "amount" => intval($_POST["amount"] * 100),
+                "currency" => "USD"
+              ]
+            ]]
+          ]
+        ]
+      );
+      $response = $checkout_api->createCheckout($location_id, $request_body);
+    } catch (Exception $e) {
+      // if an error occurs, output the message
+      echo $e->getMessage();
+      exit();
+    }
+    // this redirects to the Square hosted checkout page
+    header("Location: ".$response->getCheckout()->getCheckoutPageUrl());
+    exit();
+  } else {
+    // if no amount was set, just go back home.
+    header("Location: /");
+  }
+?>
