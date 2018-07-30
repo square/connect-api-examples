9ed93de91db9dd186da0ff2e2c35807847a6680c
Stephen Josey <sjosey@squareup.com>
2018-07-30 12:38:05 -0700

    Updated frontend to use pug instead of jade.

--- connect-examples/v2/node_payment/app.js
+++ connect-examples/v2/node_payment/app.js
@@ -13,7 +13,7 @@ var config = require('./config')[app.get('env')];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
