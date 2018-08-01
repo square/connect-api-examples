684aa1533aeb67103038f151df33f177bed5782a
Stephen Josey <sjosey@squareup.com>
2018-08-01 14:35:41 -0700

    Fixed node.js payment example (#70)
    Fixed Node JS Payment Example.

--- connect-examples/v2/node_payment/app.js
+++ connect-examples/v2/node_payment/app.js
@@ -4,14 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+squareConnect = require('square-connect');
 
 var routes = require('./routes/index');
 
 var app = express();
+var config = require('./config')[app.get('env')];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '.well-known')));
 
 app.use('/', routes);
+
+// Set Square Connect credentials
+var defaultClient = squareConnect.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: oauth2
+var oauth2 = defaultClient.authentications['oauth2'];
+oauth2.accessToken = config.squareAccessToken;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- connect-examples/v2/node_payment/routes/index.js
+++ connect-examples/v2/node_payment/routes/index.js
@@ -1,92 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 var app = express();
 var config = require('.././config.json')[app.get('env')];
 
-var unirest = require('unirest');
-var base_url = "https://connect.squareup.com/v2";
-
-// Data store for product cost
-var product_cost = {"001": 100, "002": 4900, "003": 500000};
-
-function findLocation(callback) {
-	unirest.get(base_url + '/locations')
-	.headers({
-		'Authorization': 'Bearer ' + config.squareAccessToken,
-		'Accept': 'application/json'
-	})
-	.end(function(response) {
-		for (var i = response.body.locations.length - 1; i >= 0; i--) {
-			location = response.body.locations[i];
-			if (location.capabilities && location.capabilities.indexOf("CREDIT_CARD_PROCESSING")>-1) {
-				callback(location, null);
-				return;
-			}
-			if (i==0) {
-				callback(null, {status: 400, errors: [{"detail": "No locations have credit card processing available."}]});
-			}
-		}
-	});
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	findLocation(function(location, error){
-		if (error) {
-			res.json(error);
-		} else {
-			res.render('index', {
-				'title': 'Express Node.js Implementation',
-				'square_application_id': config.squareApplicationId,
-				'square_location_id': location.id,
-				'square_location_name': location.name,
-			});
-		}
+	// Set the app and location ids for sqpaymentform.js to use
+	res.render('index', {
+		'title': 'Make Payment',
+		'square_application_id': config.squareApplicationId,
+		'square_location_id': config.squareLocationId
 	});
 });
 
-router.post('/charges/charge_card', function(req,res,next){
+router.post('/process-payment', function(req,res,next){
 	var request_params = req.body;
 
-	var token = require('crypto').randomBytes(64).toString('hex');
+	var idempotency_key = require('crypto').randomBytes(64).toString('hex');
 
-	// Check if product exists
-	if (!product_cost.hasOwnProperty(request_params.product_id)) {
-		return res.json({status: 400, errors: [{"detail": "Product Unavailable"}] })
-	}
-
-	// Make sure amount is a valid integer
-	var amount = product_cost[request_params.product_id]
-
-	// To learn more about splitting transactions with additional recipients,
-	// see the Transactions API documentation on our [developer site]
-	// (https://docs.connect.squareup.com/payments/transactions/overview#mpt-overview).
-	request_body = {
+	// Charge the customer's card
+	var transactions_api = new squareConnect.TransactionsApi();
+	var request_body = {
 		card_nonce: request_params.nonce,
 		amount_money: {
-			amount: amount,
+			amount: 100, // $1.00 charge
 			currency: 'USD'
 		},
-		idempotency_key: token
-	}
-
-	locationId = request_params.location_id;
-
-	unirest.post(base_url + '/locations/' + locationId + "/transactions")
-	.headers({
-		'Authorization': 'Bearer ' + config.squareAccessToken,
-		'Accept': 'application/json',
-		'Content-Type': 'application/json'
-	})
-	.send(request_body)
-	.end(function(response){
-		if (response.body.errors){
-			res.json({status: 400, errors: response.body.errors})
-		}else{
-			res.json({status: 200})
-		}
-	})
+		idempotency_key: idempotency_key
+	};
+	transactions_api.charge(config.squareLocationId, request_body).then(function(data) {
+		console.log(util.inspect(data, false, null));
+		res.render('process-payment', {
+			'title': 'Payment Successful',
+			'result': "Payment Successful (see console for transaction output)"
+		});
+	}, function(error) {
+		console.log(util.inspect(error.status, false, null));
+		res.render('process-payment', {
+			'title': 'Payment Failure',
+			'result': "Payment Failed (see console for error output)"
+		});
+	});
 
 });
 
