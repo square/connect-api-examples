be97735640b609d4c22dbd9e4d7caa299be0abac
Stephen Josey <sjosey@squareup.com>
2018-08-01 10:41:12 -0700

    Updated PHP payment example.

--- connect-examples/v2/php_payment/process-card.php
+++ connect-examples/v2/php_payment/process-card.php
@@ -1,13 +1,25 @@
 <?php
 
+// Note this line needs to change if you don't use Composer:
+// require('connect-php-sdk/autoload.php');
 require 'vendor/autoload.php';
+
+// dotenv is used to read from the '.env' file created for credentials
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
 # Replace these values. You probably want to start with your Sandbox credentials
 # to start: https://docs.connect.squareup.com/articles/using-sandbox/
 
 # The access token to use in all Connect API requests. Use your *sandbox* access
 # token if you're just testing things out.
-$access_token = 'REPLACE_ME';
+$access_token = ($_ENV["USE_PROD"] == 'true')  ?  $_ENV["PROD_ACCESS_TOKEN"]
+                                               :  $_ENV["SANDBOX_ACCESS_TOKEN"];
+$location_id =  ($_ENV["USE_PROD"] == 'true')  ?  $_ENV["PROD_LOCATION_ID"]
+                                               :  $_ENV["SANDBOX_LOCATION_ID"];
+
+// Initialize the authorization for Square
+\SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($access_token);
 
 # Helps ensure this code has been reached via form submission
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
@@ -25,43 +37,19 @@ if (is_null($nonce)) {
   return;
 }
 
-\SquareConnect\Configuration::getDefaultConfiguration()->setAccessToken($access_token);
-$locations_api = new \SquareConnect\Api\LocationsApi();
-
-try {
-  $locations = $locations_api->listLocations();
-  # We look for a location that can process payments
-  $location = current(array_filter($locations->getLocations(), function($location) {
-    $capabilities = $location->getCapabilities();
-    return is_array($capabilities) &&
-      in_array('CREDIT_CARD_PROCESSING', $capabilities);
-  }));
-
-} catch (\SquareConnect\ApiException $e) {
-  echo "Caught exception!<br/>";
-  print_r("<strong>Response body:</strong><br/>");
-  echo "<pre>"; var_dump($e->getResponseBody()); echo "</pre>";
-  echo "<br/><strong>Response headers:</strong><br/>";
-  echo "<pre>"; var_dump($e->getResponseHeaders()); echo "</pre>";
-  exit(1);
-}
-
 $transactions_api = new \SquareConnect\Api\TransactionsApi();
 
 # To learn more about splitting transactions with additional recipients,
 # see the Transactions API documentation on our [developer site]
 # (https://docs.connect.squareup.com/payments/transactions/overview#mpt-overview).
 $request_body = array (
-
   "card_nonce" => $nonce,
-
   # Monetary amounts are specified in the smallest unit of the applicable currency.
   # This amount is in cents. It's also hard-coded for $1.00, which isn't very useful.
   "amount_money" => array (
     "amount" => 100,
     "currency" => "USD"
   ),
-
   # Every payment you process with the SDK must have a unique idempotency key.
   # If you're unsure whether a particular payment succeeded, you can reattempt
   # it with the same idempotency key without worrying about double charging
@@ -72,7 +60,7 @@ $request_body = array (
 # The SDK throws an exception if a Connect endpoint responds with anything besides
 # a 200-level HTTP code. This block catches any exceptions that occur from the request.
 try {
-  $result = $transactions_api->charge($location->getId(), $request_body);
+  $result = $transactions_api->charge($location_id, $request_body);
   echo "<pre>";
   print_r($result);
   echo "</pre>";
