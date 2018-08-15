5483c019217c4dbd7cf9037dfad759b004b4bf9a
Stephen Josey <sjosey@squareup.com>
2018-08-15 08:49:51 -0700

    Updated styling of the form.

--- connect-examples/v2/python_payment/cgi-bin/index.py
+++ connect-examples/v2/python_payment/cgi-bin/index.py
@@ -46,62 +46,58 @@ html = """<!DOCTYPE html>
 
   <body>
     <div id="sq-ccbox">
-      <!--
-        You should replace the action attribute of the form with the path of
-        the URL you want to POST the nonce to (for example, "/process-card")
-      -->
-      <form id="nonce-form" novalidate action="/cgi-bin/process_card.py" method="post">
-
-        Pay with a Credit Card
-
-        <table>
-        <tbody>
-          <tr>
-            <td>Card Number:</td>
-            <td><div id="sq-card-number"></div></td>
-          </tr>
-          <tr>
-            <td>CVV:</td>
-            <td><div id="sq-cvv"></div></td>
-          </tr>
-          <tr>
-            <td>Expiration Date: </td>
-            <td><div id="sq-expiration-date"></div></td>
-          </tr>
-          <tr>
-            <td>Postal Code:</td>
-            <td><div id="sq-postal-code"></div></td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <button id="sq-creditcard" class="button-credit-card" onclick="requestCardNonce(event)">
-                Pay with card
-              </button>
-            </td>
-          </tr>
-        </tbody>
-        </table>
-
         <!--
-          After a nonce is generated it will be assigned to this hidden input field.
+          You should replace the action attribute of the form with the path of
+          the URL you want to POST the nonce to (for example, "/process-card")
         -->
-        <input type="hidden" id="card-nonce" name="nonce">
+        <form id="nonce-form" novalidate action="/charge" method="post">
+            <div id="error"></div>
+            <table>
+                <tbody>
+                <tr>
+                    <td class="label">Card Number:</td>
+                    <td><div id="sq-card-number"></div></td>
+                </tr>
+                <tr>
+                    <td class="label">CVV:</td>
+                    <td><div id="sq-cvv"></div></td>
+                </tr>
+                <tr>
+                    <td class="label">Expiration Date: </td>
+                    <td><div id="sq-expiration-date"></div></td>
+                </tr>
+                <tr>
+                    <td class="label">Postal Code:</td>
+                    <td><div id="sq-postal-code"></div></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button id="sq-creditcard" class="button-credit-card" onclick="requestCardNonce(event)">
+                            Pay $1.00 Now
+                        </button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
 
-      </form>
+            <!--
+              After a nonce is generated it will be assigned to this hidden input field.
+            -->
+            <input type="hidden" id="card-nonce" name="nonce">
+        </form>
     </div>
 
     <div id="sq-walletbox">
-      Pay with a Digital Wallet
+        Pay with a Digital Wallet
+        <div id="sq-apple-pay-label" class="wallet-not-enabled">Apple Pay for Web not enabled</div>
+        <!-- Placeholder for Apple Pay for Web button -->
+        <button id="sq-apple-pay" class="button-apple-pay"></button>
 
-      <div id="sq-apple-pay-label" class="wallet-not-enabled">Apple Pay for Web not enabled</div>
-      <!-- Placholder for Apple Pay for Web button -->
-      <button id="sq-apple-pay" class="button-apple-pay"></button>
-
-      <div id="sq-masterpass-label" class="wallet-not-enabled">Masterpass not enabled</div>
-      <!-- Placholder for Masterpass button -->
-      <button id="sq-masterpass" class="button-masterpass"></button>
+        <div id="sq-masterpass-label" class="wallet-not-enabled">Masterpass not enabled</div>
+        <!-- Placeholder for Masterpass button -->
+        <button id="sq-masterpass" class="button-masterpass"></button>
     </div>
-  </body>
+</body>
 </html>
 """
 print(html)
