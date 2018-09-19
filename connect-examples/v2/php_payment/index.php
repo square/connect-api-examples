0ff69270b6f29b9096105912f489e94321606fdc
Stephen Josey <sjosey@squareup.com>
2018-09-19 13:15:35 -0700

    Updated index for PHP example.

--- connect-examples/v2/php_payment/index.php
+++ connect-examples/v2/php_payment/index.php
@@ -27,63 +27,68 @@ $dotenv->load();
   </script>
 
   <!-- link to the local SqPaymentForm initialization -->
-  <script type="text/javascript" src="/sqpaymentform.js"></script>
+  <script type="text/javascript" src="/sq-payment-form.js"></script>
 
   <!-- link to the custom styles for SqPaymentForm -->
-  <link rel="stylesheet" type="text/css" href="/sqpaymentform.css">
+  <link rel="stylesheet" type="text/css" href="/sq-payment-form.css">
 </head>
 <body>
-  <div id="sq-ccbox">
+  <!-- Begin Payment Form -->
+  <div class="sq-payment-form">
     <!--
-      You should replace the action attribute of the form with the path of
-      the URL you want to POST the nonce to (for example, "/process-card")
+      Square's JS will automatically hide these buttons if they are unsupported
+      by the current device.
     -->
-    <form id="nonce-form" novalidate action="/process-card.php" method="post">
-      <div id="error"></div>
-      <table>
-      <tbody>
-        <tr>
-          <td class="label">Card Number:</td>
-          <td><div id="sq-card-number"></div></td>
-        </tr>
-        <tr>
-          <td class="label">CVV:</td>
-          <td><div id="sq-cvv"></div></td>
-        </tr>
-        <tr>
-          <td class="label">Expiration Date: </td>
-          <td><div id="sq-expiration-date"></div></td>
-        </tr>
-        <tr>
-          <td class="label">Postal Code:</td>
-          <td><div id="sq-postal-code"></div></td>
-        </tr>
-        <tr>
-          <td colspan="2">
-            <button id="sq-creditcard" class="button-credit-card" onclick="requestCardNonce(event)">
-              Pay $1.00 Now
-            </button>
-          </td>
-        </tr>
-      </tbody>
-      </table>
-
+    <div id="sq-walletbox">
+      <button id="sq-apple-pay" class="sq-apple-pay"></button>
+      <button id="sq-masterpass" class="sq-masterpass"></button>
+      <div class="sq-wallet-divider">
+        <span class="sq-wallet-divider__text">Or</span>
+      </div>
+    </div>
+    <div id="sq-ccbox">
       <!--
-        After a nonce is generated it will be assigned to this hidden input field.
+        You should replace the action attribute of the form with the path of
+        the URL you want to POST the nonce to (for example, "/process-card").
+
+        You need to then make a "Charge" request to Square's transaction API with
+        this nonce to securely charge the customer.
+
+        Learn more about how to setup the server component of the payment form here:
+        https://docs.connect.squareup.com/payments/transactions/processing-payment-rest
       -->
-      <input type="hidden" id="card-nonce" name="nonce">
-    </form>
+      <form id="nonce-form" novalidate action="/process-card.php">
+        <div class="sq-field">
+          <label class="sq-label">Card Number</label>
+          <div id="sq-card-number"></div>
+        </div>
+        <div class="sq-field-wrapper">
+          <div class="sq-field sq-field--in-wrapper">
+            <label class="sq-label">CVV</label>
+            <div id="sq-cvv"></div>
+          </div>
+          <div class="sq-field sq-field--in-wrapper">
+            <label class="sq-label">Expiration</label>
+            <div id="sq-expiration-date"></div>
+          </div>
+          <div class="sq-field sq-field--in-wrapper">
+            <label class="sq-label">Postal</label>
+            <div id="sq-postal-code"></div>
+          </div>
+        </div>
+        <div class="sq-field">
+          <button id="sq-creditcard" class="sq-button" onclick="requestCardNonce(event)">
+            Pay $1.00 Now
+          </button>
+        </div>
+        <!--
+          After a nonce is generated it will be assigned to this hidden input field.
+        -->
+        <div id="error"></div>
+        <input type="hidden" id="card-nonce" name="nonce">
+      </form>
+    </div>
   </div>
-
-  <div id="sq-walletbox">
-    Pay with a Digital Wallet
-    <div id="sq-apple-pay-label" class="wallet-not-enabled">Apple Pay for Web not enabled</div>
-    <!-- Placeholder for Apple Pay for Web button -->
-    <button id="sq-apple-pay" class="button-apple-pay"></button>
-
-    <div id="sq-masterpass-label" class="wallet-not-enabled">Masterpass not enabled</div>
-    <!-- Placeholder for Masterpass button -->
-    <button id="sq-masterpass" class="button-masterpass"></button>
-  </div>
+  <!-- End Payment Form -->
 </body>
 </html>
