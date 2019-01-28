9221a467dd7c022547b9a06e0f58f6698c963c26
bwisem <32969758+bwisem@users.noreply.github.com>
2019-01-28 10:11:31 -0800

    Fixed transaction and error output (#119)

--- connect-examples/v2/php_payment/index.php
+++ connect-examples/v2/php_payment/index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 // dotenv is used to read from the '.env' file created for credentials
-$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 ?>
 <html>
