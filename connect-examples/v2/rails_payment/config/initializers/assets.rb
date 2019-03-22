5dd0d7d37c6b720a8fa20d16d3a4a5161d95ec94
Xiao Hu <xiao@squareup.com>
2019-03-22 11:17:09 -0700

    Udpate rails example to incorporate application flow

--- connect-examples/v2/rails_payment/config/initializers/assets.rb
+++ connect-examples/v2/rails_payment/config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( sq-payment-form.css )
+Rails.application.config.assets.precompile += %w( sq-payment-form.js )
