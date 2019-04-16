343994ffc1bb3c55bbba84871b20cf350af204ac
Stephen Josey <sjosey@squareup.com>
2019-04-16 16:12:25 -0700

    Started C# checkout sample

---
+++ connect-examples/v2/csharp_checkout/csharp_checkout/Pages/Checkout.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Square.Connect.Api;
+using Square.Connect.Model;
+
+namespace csharp_checkout.Pages
+{
+    public class CheckoutModel : PageModel
+    {
+        private readonly string locationId;
+
+        public CheckoutModel(IConfiguration configuration)
+        {
+            this.locationId = configuration["AppSettings:LocationId"];
+        }
+
+        public string ResultMessage
+        {
+            get;
+            set;
+        }
+
+        public IActionResult OnPost()
+        {
+            CheckoutApi checkoutApi = new CheckoutApi();
+            int amount = (int) float.Parse(Request.Form["amount"]) * 100;
+
+            try
+            {
+
+                CreateCheckoutRequest body = new CreateCheckoutRequest(
+                    IdempotencyKey: Guid.NewGuid().ToString(),
+                    Order: new CreateOrderRequest(
+                        LineItems: new List<CreateOrderRequestLineItem>()
+                        {
+                            new CreateOrderRequestLineItem(
+                                Name: "Test Payment",
+                                Quantity: "1",
+                                BasePriceMoney: new Money(Amount: 500,
+                                                          Currency: Money.CurrencyEnum.USD)
+                            )
+                        }
+                ));
+
+                CreateCheckoutResponse response = checkoutApi.CreateCheckout(locationId, body);
+                return Redirect(response.Checkout.CheckoutPageUrl);
+            }
+            catch (Square.Connect.Client.ApiException e)
+            {
+                this.ResultMessage = e.Message;
+                return null;
+            }
+        }
+    }
+}
