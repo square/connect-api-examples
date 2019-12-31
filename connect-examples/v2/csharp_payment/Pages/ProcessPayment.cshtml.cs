28f5b85d19300407a418b4f9cbb5bbc1db850891
Jessie De La Cruz Santos <jessiedlcs@squareup.com>
2019-12-30 21:20:53 -0500

    Upgraded the payment example to latest SDK

--- connect-examples/v2/csharp_payment/Pages/ProcessPayment.cshtml.cs
+++ connect-examples/v2/csharp_payment/Pages/ProcessPayment.cshtml.cs
@@ -1,36 +1,37 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
-using Square.Connect.Api;
-using Square.Connect.Client;
-using Square.Connect.Model;
+using Square;
+using Square.Models;
+using Square.Apis;
+using Square.Exceptions;
 
 namespace sqRazorSample.Pages
 {
     public class ProcessPaymentModel : PageModel
     {
-        readonly Configuration configuration;
-
+        private SquareClient client;
         public string ResultMessage
         {
             get;
             set;
         }
 
-        public ProcessPaymentModel(IConfiguration configuration) {
-            // Set 'basePath' to switch between sandbox env and production env
-            // sandbox: https://connect.squareupsandbox.com
-            // production: https://connect.squareup.com
-            string basePath = configuration["AppSettings:Environment"] == "sandbox" ?
-                "https://connect.squareupsandbox.com" : "https://connect.squareup.com";
-            this.configuration = new Configuration(new ApiClient(basePath));
-            this.configuration.AccessToken = configuration["AppSettings:AccessToken"];
+        public ProcessPaymentModel(Microsoft.Extensions.Configuration.IConfiguration configuration) {
+            // Get environment
+            Square.Environment environment = configuration["AppSettings:Environment"] == "sandbox" ?
+                Square.Environment.Sandbox : Square.Environment.Production;
+
+            // Build base client
+            client = new SquareClient.Builder()
+                .Environment(environment)
+                .AccessToken(configuration["AppSettings:AccessToken"])
+                .Build();
         }
 
         public void OnPost()
         {
             string nonce = Request.Form["nonce"];
-            PaymentsApi paymentsApi = new PaymentsApi(configuration: this.configuration);
+            IPaymentsApi PaymentsApi = client.PaymentsApi;
             // Every payment you process with the SDK must have a unique idempotency key.
             // If you're unsure whether a particular payment succeeded, you can reattempt
             // it with the same idempotency key without worrying about double charging
@@ -40,17 +41,22 @@ namespace sqRazorSample.Pages
             // Monetary amounts are specified in the smallest unit of the applicable currency.
             // This amount is in cents. It's also hard-coded for $1.00,
             // which isn't very useful.
-            Money amount = new Money(100, "USD");
+            Money amount = new Money.Builder()
+                .Amount(500L)
+                .Currency("USD")
+                .Build();
 
             // To learn more about splitting payments with additional recipients,
             // see the Payments API documentation on our [developer site]
             // (https://developer.squareup.com/docs/payments-api/overview).
-            CreatePaymentRequest createPaymentRequest = new CreatePaymentRequest(AmountMoney: amount, IdempotencyKey: uuid, SourceId: nonce);
+            CreatePaymentRequest createPaymentRequest  = new CreatePaymentRequest.Builder(nonce,uuid,amount)
+                .Note("From Square Sample Csharp App")
+                .Build();
 
             try
             {
-                var response = paymentsApi.CreatePayment(createPaymentRequest);
-                this.ResultMessage = "Payment complete! " + response.ToJson();
+                CreatePaymentResponse response = PaymentsApi.CreatePayment(createPaymentRequest);
+                this.ResultMessage = "Payment complete! " + response.Payment.Note;
             }
             catch (ApiException e)
             {
