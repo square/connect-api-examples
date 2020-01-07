e73c7a8260e2f8e65c944ca60951b721cc2363e9
Stephen Josey <sjosey@squareup.com>
2020-01-07 13:24:09 -0800

    Fixed spacing

--- connect-examples/oauth/php/index.php
+++ connect-examples/oauth/php/index.php
@@ -10,5 +10,5 @@ $appId = ($_ENV["USE_PROD"] == 'true') ? $_ENV["PROD_APP_ID"]
                                         : $_ENV["SANDBOX_APP_ID"];
 
 echo ($_ENV["USE_PROD"] == 'true') ? "<a href=\"https://connect.squareup.com/oauth2/authorize?client_id=$appId\">Click here</a> to authorize the application."
-                                    : "<a href=\"https://connect.squareupsandbox.com/oauth2/authorize?client_id=$appId\">Click here</a> to authorize the application.";
+                                   : "<a href=\"https://connect.squareupsandbox.com/oauth2/authorize?client_id=$appId\">Click here</a> to authorize the application.";
 ?>
