ef70a7618e72d25cba47d3113dcfc2ce05a6932c
Richard Moot <r@moot.dev>
2020-05-12 17:36:28 -0700

    Change example to use flask and new python SDK

--- connect-examples/oauth/python/README.md
+++ connect-examples/oauth/python/README.md
@@ -12,24 +12,24 @@ Open your terminal at this directory and type:
 pip install git+https://github.com/square/connect-python-sdk.git
 ```
 
-### Install Bottle
+### Install Flask
 
-This application requires Python and the Bottle web framework. If you have Python
-installed, you can install Bottle by following
-[these steps](http://bottlepy.org/docs/dev/tutorial.html#installation).
+This application requires Python and the Flask web framework. If you have Python
+installed, you can install Flask by following
+[these steps](https://flask.palletsprojects.com/en/1.1.x/installation/).
 
 ### Specify your application credentials
 
 In order for the sample to work, you must specify two fields in `oauth-flow.py`:
 
 * Replace the value of `application_id` with your application's ID, available on your
-[application dashboard](https://connect.squareup.com/apps).
+[application dashboard](https://developer.squareup.com/apps).
 
 * Replace the value of `application_secret` with your application's secret, also available on your application dashboard.
 
 ### Set your application's Redirect URL
 
-On your application dashboard, set your application's Redirect URL to `http://localhost:8080/callback`.
+On your application dashboard, set your application's Redirect URL to `http://127.0.0.1:5000/callback`.
 
 Note that applications that don't use a `localhost` URL must use HTTPS. HTTP is allowed for `localhost` URLs to simplify the development process.
 
@@ -37,6 +37,6 @@ Note that applications that don't use a `localhost` URL must use HTTPS. HTTP is 
 
 To run the example, execute the following from the command line:
 
-    python oauth-flow.py
+    FLASK_APP=oauth-flow.py flask run
 
-You can then proceed through the OAuth flow by going to `http://localhost:8080` in your web browser.
+You can then proceed through the OAuth flow by going to `http://127.0.0.1:5000/` in your web browser.
--- connect-examples/oauth/python/oauth-flow.py
+++ connect-examples/oauth/python/oauth-flow.py
@@ -11,20 +11,22 @@
 # 5. The Obtain Token endpoint returns an access token your application can use in subsequent requests
 #    to the Connect API.
 
-from bottle import get, request, static_file, run, response
-import squareconnect
-from squareconnect.apis.o_auth_api import OAuthApi
-from squareconnect.models.obtain_token_request import ObtainTokenRequest
+from flask import Flask, request
+from square.client import Client
+
+client = Client(
+  environment="production"
+)
+obtain_token = client.o_auth.obtain_token
+
+app = Flask(__name__)
 
 # Your application's ID and secret, available from your application dashboard.
 application_id = 'REPLACE_ME'
 application_secret = 'REPLACE_ME'
 
-# OAuth API instance
-oauth_api = OAuthApi()
-
 # Serves the link that merchants click to authorize your application
-@get('/')
+@app.route('/', methods=['GET'])
 def authorize():
   return '''<a href="https://connect.squareup.com/oauth2/authorize?client_id={0}">Click here</a>
             to authorize the application.'''.format(application_id)
@@ -32,27 +34,27 @@ def authorize():
 # Serves requsts from Square to your application's redirect URL
 # Note that you need to set your application's Redirect URL to
 # http://localhost:8080/callback from your application dashboard
-@get('/callback')
+@app.route('/callback', methods=['GET'])
 def callback():
 
   # Extract the returned authorization code from the URL
-  authorization_code = request.query.get('code')
+  authorization_code = request.args.get('code')
   if authorization_code:
 
     # Provide the code in a request to the Obtain Token endpoint
-    oauth_request_body = ObtainTokenRequest()
-    oauth_request_body.client_id = application_id
-    oauth_request_body.client_secret = application_secret
-    oauth_request_body.code = authorization_code
-    oauth_request_body.grant_type = 'authorization_code'
+    body = {}
+    body['client_id'] = application_id
+    body['client_secret'] = application_secret
+    body['code'] = authorization_code
+    body['grant_type'] = 'authorization_code'
 
-    response = oauth_api.obtain_token(oauth_request_body)
+    response = obtain_token(body)
 
-    if response.access_token:
+    if response.body:
 
       # Here, instead of printing the access token, your application server should store it securely
       # and use it in subsequent requests to the Connect API on behalf of the merchant.
-      print ('Access token: ' + response.access_token)
+      print (response.body)
       return 'Authorization succeeded!'
 
     # The response from the Obtain Token endpoint did not include an access token. Something went wrong.
@@ -62,6 +64,3 @@ def callback():
   # The request to the Redirect URL did not include an authorization code. Something went wrong.
   else:
     return 'Authorization failed!'
-
-# Start up the server
-run(host='localhost', port=8080)
