9270e4b404fb0709c3883387c53009e8326cd723
Dean Papastrat <dpapastrat@squareup.com>
2021-05-12 09:29:12 -0700

    Hide ACH payment method unless it's enabled

--- connect-examples/v2/node_payment/public/js/sq-ach.js
+++ connect-examples/v2/node_payment/public/js/sq-ach.js
@@ -1,17 +1,20 @@
 async function ACHPay(buttonEl) {
   const accountHolderNameEl = document.getElementById('ach-account-holder-name');
   const achMessageEl = document.getElementById("ach-message");
+  const achWrapperEl = document.getElementById("ach-wrapper");
+
   let ach;
   try {
     ach = await window.payments.ach();
+    achWrapperEl.style.display = 'block';
   } catch (e) {
-    // If the ACH payment method is not supported by your account then 
+    // If the ACH payment method is not supported by your account then
     // do not enable the #ach-account-holder-name input field
     if (e.name === 'PaymentMethodUnsupportedError') {
       achMessageEl.innerText = "ACH payment is not supported by your account";
       accountHolderNameEl.disabled = true;
     }
-    
+
     // if we can't load ACH, we shouldn't bind events for the button
     return;
   }
