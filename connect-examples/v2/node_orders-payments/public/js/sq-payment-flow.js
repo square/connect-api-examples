8de984e81c980fd3483a872c8401709050b5de06
Emma Chen <emmac@squareup.com>
2021-05-11 23:26:18 -0600

    address best practice problems

--- connect-examples/v2/node_orders-payments/public/js/sq-payment-flow.js
+++ connect-examples/v2/node_orders-payments/public/js/sq-payment-flow.js
@@ -1,6 +1,5 @@
 async function SquarePaymentFlow() {
   // Create card payment object and attach to page
-  console.log(document.getElementById('card-container'));
   window.CardPay(document.getElementById('card-container'));
 }
 
@@ -12,25 +11,28 @@ window.createPayment = async function createPayment(token) {
     idempotencyKey: document.getElementById('idempotencyKey').value
   });
 
-  fetch('payment', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: dataJsonString
-  })
-  .then(response => response.json())
-  .then(data => {
+  try {
+    const response = await fetch('payment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: dataJsonString
+    });
+    const data = await response.json();
     console.log(data);
-    if (data.errors) {
-      document.getElementById('message').innerHTML = data.errors[0].detail;
+    if (data.errors && data.errors.length > 0) {
+      if (data.errors[0].detail) {
+        document.getElementById('message').innerHTML = data.errors[0].detail;
+      } else {
+        document.getElementById('message').innerHTML = 'Payment failed.'
+      }
     } else {
       window.location.href = `/order-confirmation?orderId=${document.getElementById('orderId').value}&locationId=${document.getElementById('locationId').value}`;
     }
-  })
-  .catch((error) => {
+  } catch(error) {
     console.error('Error:', error);
-  });
+  };
 }
 
 SquarePaymentFlow();
