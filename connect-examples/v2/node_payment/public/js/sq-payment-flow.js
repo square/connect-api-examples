34d0263185de1c2c012aa07d686f1fa9da74e38a
Wolfgang Schuster <wolfgang@squareup.com>
2021-05-12 16:01:46 -0600

    Fix errors typo

--- connect-examples/v2/node_payment/public/js/sq-payment-flow.js
+++ connect-examples/v2/node_payment/public/js/sq-payment-flow.js
@@ -44,8 +44,8 @@ window.createPayment = async function(token) {
     });
 
     const data = await response.json();
-    
-    if (data.errors && data.error.length > 0) {
+
+    if (data.errors && data.errors.length > 0) {
       if (data.errors[0].detail) {
         window.showError(data.errors[0].detail);
       } else {
