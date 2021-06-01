6c48327cef648601dccec4424247d289aa7645a3
Emma Chen <emmac@squareup.com>
2021-05-31 18:17:50 -0600

    add some more comments

--- util/middleware.js
+++ util/middleware.js
@@ -10,15 +10,18 @@ async function checkLoginStatus(req, res, next) {
   }
 }
 
+/**
+ * If the gift card being accessed does not belong to the customer 
+ * logged in, redirect to the login page
+ * */ 
 async function checkCardOwner(req, res, next) {
   if (req.params.gan) {
-    // If the gift card being accessed does not belong to the customer
-    // logged in, redirect to the login page
     const gan = req.params.gan;
     const { result: { giftCard } } = await giftCardsApi.retrieveGiftCardFromGAN({ gan });
     if (!giftCard.customerIds.includes(req.session.customerId)) {
       res.redirect("/login");
     } else {
+      // pass the giftCard object to the next middleware
       res.locals.giftCard = giftCard;
       next();
     }
