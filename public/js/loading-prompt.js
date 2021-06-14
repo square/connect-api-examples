2df827bef421c44b33bea449f41a8ed76020aba9
Barak Jacob <barak@squareup.com>
2021-06-14 13:22:05 -0700

    Address some comments

--- public/js/loading-prompt.js
+++ public/js/loading-prompt.js
@@ -3,6 +3,5 @@ function createLoadingPrompt(formElementId) {
   document.getElementById(formElementId).submit();
 
   // Create the loading screen by exposing the prompt and shading the background.
-  document.getElementsByClassName('shadow-overlay')[0].style.display='flex';
-  document.getElementsByClassName('loading_prompt-container')[0].style.display='flex';
+  document.getElementsByClassName('loading__shadow-overlay')[0].style.display='flex';
 }
