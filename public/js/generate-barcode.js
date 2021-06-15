d0ba7e5ea737a5949f206524da868f0b875fee9e
Barak Jacob <barak@squareup.com>
2021-06-15 13:30:51 -0700

    Update public/js/generate-barcode.js
    Co-authored-by: Emma Chen <79542071+emmac3@users.noreply.github.com>

--- public/js/generate-barcode.js
+++ public/js/generate-barcode.js
@@ -3,7 +3,7 @@ function generateBarCode(elementId, value) {
   let canvas = document.createElement('canvas');
   let options = {
     bcid:        'pdf417',        // Barcode type
-    text:        squareGiftCardBarcodeId.toString(),    // Text to encode
+    text:        squareGiftCardBarcodeId,    // Text to encode
     scale:       2,               // 3x scaling factor
     height:      10,              // Bar height, in millimeters
     includetext: true,            // Show human-readable text
