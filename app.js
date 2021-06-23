40f82e7836ab6295b97c7c27b7e71c66e9d36d1d
Amber Lee <amberl@squareup.com>
2021-06-22 18:00:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,48 +13,48 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const express = require("express",);
-const logger = require("morgan",);
-const cookieParser = require("cookie-parser",);
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
-const routes = require("./routes/index",);
+const routes = require("./routes/index");
 const app = express();
 
-require("./util/square-client",);
+require("./util/square-client");
 
 // set the view engine to ejs
-app.set("view engine", "ejs",);
+app.set("view engine", "ejs");
 
-app.use(logger("dev",),);
+app.use(logger("dev"));
 
-app.use(express.static(__dirname + "/public",),);
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.json(),);
+app.use(express.json());
 app.use(express.urlencoded({
-  extended: false,
-},),);
+  extended: false
+}));
 
-app.use(cookieParser(),);
-app.use("/", routes,);
+app.use(cookieParser());
+app.use("/", routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next,) {
-  const err = new Error("Not Found",);
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
-  next(err,);
-},);
+  next(err);
+});
 
 // error handlers
 // For simplicity, we print all error information
-app.use(function (err, req, res, next,) {
-  res.status(err.status || 500,);
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
   res.render("pages/error", {
     // If it is a response error then format the JSON string, if not output the error
-    error: err.errors ? JSON.stringify(err.errors, null, 4,) : err.stack,
+    error: err.errors ? JSON.stringify(err.errors, null, 4) : err.stack,
     message: err.message,
     status: err.status,
-  },);
-},);
+  });
+});
 
 
 module.exports = app;
