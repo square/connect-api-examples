d57984f3cc81dbe06720d3e90c9c86451350b715
Emma Chen <emmac@squareup.com>
2021-07-05 16:28:50 -0600

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -13,13 +13,10 @@ limitations under the License.
 
 const express = require("express");
 const router = express.Router();
-const bookingRoute = require("./booking");
-
-router.use("/booking", bookingRoute);
-
 const servicesRoute = require("./services");
 const staffRoute = require("./staff");
 const bookingRoute = require("./booking");
+
 router.use("/services", servicesRoute);
 router.use("/staff", staffRoute);
 router.use("/booking", bookingRoute);
